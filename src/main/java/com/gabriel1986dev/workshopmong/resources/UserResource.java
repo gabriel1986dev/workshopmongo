b2506be518f49f567a667114ed9aaaeed0f7b704
gabriel1986dev <etevaldo1986@yahoo.com>
2024-06-25 14:17:59 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/gabriel1986dev/workshopmong/resources/UserResource.java
+++ src/main/java/com/gabriel1986dev/workshopmong/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gabriel1986dev.workshopmong.domain.Post;
 import com.gabriel1986dev.workshopmong.domain.User;
 import com.gabriel1986dev.workshopmong.dto.UserDTO;
 import com.gabriel1986dev.workshopmong.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj =service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
